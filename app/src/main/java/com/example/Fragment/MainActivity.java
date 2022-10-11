9ff5051a78a268157dfa22f455413b3a012514f8
Kaunain <kaunainreza27@gmail.com>
2022-10-11 11:50:10 +0530

    added camera

--- app/src/main/java/com/example/Fragment/MainActivity.java
+++ app/src/main/java/com/example/Fragment/MainActivity.java
@@ -6,15 +6,21 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.fragment.R;
 
 public class MainActivity extends AppCompatActivity {
+   // private final int CAMERA_REQ_CODE=100;
     Button firstFragment, secondFragment,webPageBtn;
+    ImageButton imageButton;
+    //ImageView imageViewCamera;
 
 
     @Override
@@ -23,6 +29,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         firstFragment = (Button) findViewById(R.id.firstFragment);
         secondFragment = (Button) findViewById(R.id.secondFragment);
+
+        //imageViewCamera =(ImageView)findViewById(R.id.imageCamera);
+        imageButton = (ImageButton) findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+               startActivity(intent);
+            }
+        });
+
         webPageBtn= (Button)findViewById(R.id.webPageBtn);
         webPageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/Fragment/WebViewActivity.java
+++ app/src/main/java/com/example/Fragment/WebViewActivity.java
@@ -2,24 +2,44 @@ package com.example.Fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
+
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import com.example.fragment.R;
 
 public class WebViewActivity extends AppCompatActivity {
-    WebView  webView;
 
-    @SuppressLint("MissingInflatedId")
+   public WebView  webView;
+   public ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
+        progressBar=(ProgressBar)findViewById(R.id.progressBarBtn);
         webView = findViewById(R.id.webView);
-        webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progressBar.setVisibility(view.VISIBLE);
+                setTitle("Loading...");
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(view.GONE);
+                setTitle(view.getTitle());
+            }
+        });
+
         webView.loadUrl("https://abhiandroid.com/ui/");
+
+
 
     }
 
